a09a3379f2209653cbbcfd69103341c2c9fa161b
ortemmetro <ortem.koldun@gmail.com>
2023-11-16 17:02:01 +0600

    updated flutter and packages

--- lib/widgets/expenses_page_widget/expenses_page_widget.dart
+++ lib/widgets/expenses_page_widget/expenses_page_widget.dart
@@ -57,12 +57,13 @@ class _ExpensesPageWidgetState extends State<ExpensesPageWidget>
                   bottom: 0.h,
                   right: 0.w,
                   child: RawMaterialButton(
-                    onPressed: () =>
-                        Navigator.of(context).pushNamed('/main_page/add',
-                            arguments: ExpenseInfo(
-                              listOfExpenses: [],
-                              category: "expense",
-                            )),
+                    onPressed: () => Navigator.of(context).pushNamed(
+                      '/main_page/add',
+                      arguments: ExpenseInfo(
+                        listOfExpenses: [],
+                        category: "expense",
+                      ),
+                    ),
                     fillColor: const Color.fromARGB(255, 93, 176, 117),
                     shape: const CircleBorder(),
                     padding: EdgeInsets.symmetric(
